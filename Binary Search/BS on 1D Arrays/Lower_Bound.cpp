efe4afe4438cadc406181de31cb83b4081926d3b
YashKirnapure77 <yashkirnapure777@gmail.com>
2025-08-03 17:25:27 +0530

    .

---
+++ Binary Search/BS on 1D Arrays/Lower_Bound.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int lowerBound(vector<int>&arr,int x)
+{
+    int n = arr.size();
+    int low = 0;
+    int high = n-1;
+    int ans = n;
+
+    while(low <= high)
+    {
+        int mid = (low + high)/2;
+        if(arr[mid] >= x)
+        {
+            ans = mid;
+            high = mid - 1; //look for more small index on left
+        }
+        else
+            low = mid + 1;
+    }
+    return ans;
+}
+
+// int lb = lower_bound(arr.begin(),arr.end(),x) - arr.begin();
---
+++ Binary Search/BS on 1D Arrays/Upper_Bound.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int upperBound(vector<int>&arr,int x)
+{
+    int n = arr.size();
+    int low = 0;
+    int high = n-1;
+    int ans = n;
+
+    while(low <= high)
+    {
+        int mid = (low + high)/2;
+        if(arr[mid] > x)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+            low = mid + 1;
+    }
+    return ans;
+}
+
+// int ub = upper_bound(arr.begin(),arr.end(),x) - arr.begin();
