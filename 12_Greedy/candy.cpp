7e9fbef2166e559a25904fc9495594a4db06988a
YashKirnapure77 <yashkirnapure777@gmail.com>
2025-10-21 22:57:56 +0530

    POTD

---
+++ 12_Greedy/candy.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int candy(vector<int> &ratings)
+{
+    int n = ratings.size();
+    int sum = 1;
+    int i = 1;
+
+    while (i < n)
+    {
+        if (ratings[i] == ratings[i - 1])
+        {
+            sum = sum + 1;
+            i++;
+            continue;
+        }
+
+        int peak = 1;
+
+        while (i < n && ratings[i] > ratings[i - 1])
+        {
+            peak++;
+            sum += peak;
+            i++;
+        }
+
+        int down = 1;
+
+        while (i < n && ratings[i] < ratings[i - 1])
+        {
+            sum += down;
+            down++;
+            i++;
+        }
+
+        if (down > peak)
+            sum += down - peak;
+    }
+    return sum;
+}
---
+++ POTD/21-10-2025.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int maxFrequency(vector<int> &nums, int k, int numOperations)
+{
+    int n = nums.size();
+    int maxEl = *max_element(begin(nums), end(nums)) + k;
+
+    vector<int> freq(maxEl + 1, 0);
+
+    for (int x : nums)
+        freq[x]++;
+
+    // cumulative sum of freq
+    for (int i = 1; i <= maxEl; i++)
+        freq[i] += freq[i - 1];
+
+    int result = 0;
+    for (int target = 0; target <= maxEl; target++)
+    {
+        if (freq[target] == 0)
+            continue;
+
+        int leftNum = max(0, target - k);
+        int rightNum = min(maxEl, target + k);
+
+        int totalCount = freq[rightNum] - (leftNum > 0 ? freq[leftNum - 1] : 0);
+
+        int targetCount = freq[target] - (target > 0 ? freq[target - 1] : 0);
+
+        int needConversion = totalCount - targetCount;
+
+        int maxPossibleFreq = targetCount + min(needConversion, numOperations);
+
+        result = max(result, maxPossibleFreq);
+    }
+    return result;
+}
