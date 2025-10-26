4675f7a3d5e0ad0b84765d90996a4aa3dad3e796
YashKirnapure77 <yashkirnapure777@gmail.com>
2025-10-26 22:43:58 +0530

    continued

---
+++ 13_Binary_Trees/Medium/isIdentical.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isSameTree(TreeNode *p, TreeNode *q)
+{
+    if (!p && q)
+        return false;
+    if (p && !q)
+        return false;
+    if (!p && !q)
+        return true;
+    if (p->val != q->val)
+        return false;
+    return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+}
